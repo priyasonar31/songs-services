f5eb5b893cb987260a72fac71290f2b51ba28503
unknown <priyasonar.2009@gmail.com>
2024-07-22 21:59:04 +0530

    test working

--- functions/api.js
+++ functions/api.js
@@ -1,15 +1,15 @@
 import express from "express";
 import cors from 'cors';
-import  Top5Router  from "./routes/Top5Router.js";
-import GenAiRouter from "./routes/genAiRouter.js";
+// import Top5Router from "./routes/Top5Router.js";
+// import GenAiRouter from "./routes/genAiRouter.js";
 import ServerlessHttp from "serverless-http";
 import dotenv from 'dotenv';
 
 dotenv.config();
 app.use(cors());
 app.use(express.json());
-app.use('/.netlify/functions/api/top5', Top5Router);
-app.use('/.netlify/functions/api/gen-ai', GenAiRouter);
+// app.use('/.netlify/functions/api/top5', Top5Router);
+// app.use('/.netlify/functions/api/gen-ai', GenAiRouter);
 app.get('/.netlify/functions/api/hello-world', (req, res) => {
     return res.json({
         messages: "hello world!"
@@ -18,7 +18,7 @@ app.get('/.netlify/functions/api/hello-world', (req, res) => {
 
 const handler = ServerlessHttp(app);
 
-module.exports.handler = async(event, context) => {
+module.exports.handler = async (event, context) => {
     const result = await handler(event, context);
     return result;
 }
