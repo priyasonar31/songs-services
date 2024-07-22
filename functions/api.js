122ed05d75927ac7c1420cdc0d9669676bf2e15c
unknown <priyasonar.2009@gmail.com>
2024-07-22 21:30:41 +0530

    revert route changes

--- functions/api.js
+++ functions/api.js
@@ -1,15 +1,20 @@
 import express from "express";
+import cors from 'cors';
+import  Top5Router  from "./routes/Top5Router.js";
+import GenAiRouter from "./routes/genAiRouter.js";
 import ServerlessHttp from "serverless-http";
+import dotenv from 'dotenv';
 
-const app = express();
-
-
-app.get('/.netlify/functions/api', (req, res) => {
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+app.use('/.netlify/functions/api/top5', Top5Router);
+app.use('/.netlify/functions/api/gen-ai', GenAiRouter);
+app.get('/.netlify/functions/api/hello-world', (req, res) => {
     return res.json({
         messages: "hello world!"
     })
-})
-
+});
 
 const handler = ServerlessHttp(app);
 
