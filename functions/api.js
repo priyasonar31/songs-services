1be1e2e2de2d45d4f87f1663e4560aa118af6397
unknown <priyasonar.2009@gmail.com>
2024-07-22 21:26:05 +0530

    testing changes

--- functions/api.js
+++ functions/api.js
@@ -1,21 +1,14 @@
 import express from "express";
-import cors from 'cors';
-import  Top5Router  from "./routes/Top5Router.js";
-import GenAiRouter from "./routes/genAiRouter.js";
 import ServerlessHttp from "serverless-http";
-import dotenv from 'dotenv';
-dotenv.config();
 
-export const app = express();
-app.use(cors());
-app.use(express.json());
-app.use('/.netlify/functions/api/top5', Top5Router);
-app.use('/.netlify/functions/api/gen-ai', GenAiRouter);
-app.get('/.netlify/functions/api/hello-world', (req, res) => {
+const app = express();
+
+
+app.get('/.netlify/functions/api', (req, res) => {
     return res.json({
         messages: "hello world!"
     })
-});
+})
 
 
 const handler = ServerlessHttp(app);
--- server-local.js
+++
@@ -1,3 +0,0 @@
-import { app } from './functions/api.js';
-
-app.listen(5001, () => console.log('Local app listening on port 5001!'));
