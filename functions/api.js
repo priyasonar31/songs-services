b8010646c0cec99d0623575da7349cc50b7d00ed
unknown <priyasonar.2009@gmail.com>
2024-07-22 22:37:46 +0530

    testing changes 3

--- functions/api.js
+++ functions/api.js
@@ -1,15 +1,15 @@
 import express from "express";
 import cors from 'cors';
-// import Top5Router from "./routes/Top5Router.js";
-// import GenAiRouter from "./routes/genAiRouter.js";
+import Top5Router from "./routes/Top5Router.js";
+import GenAiRouter from "./routes/genAiRouter.js";
 import ServerlessHttp from "serverless-http";
 import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-// app.use('/.netlify/functions/api/top5', Top5Router);
-// app.use('/.netlify/functions/api/gen-ai', GenAiRouter);
+app.use('/.netlify/functions/api/top5', Top5Router);
+app.use('/.netlify/functions/api/gen-ai', GenAiRouter);
 app.get('/.netlify/functions/api/hello-world', (req, res) => {
     return res.json({
         messages: "hello world!"
