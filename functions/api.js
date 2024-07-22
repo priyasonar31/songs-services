17fb211d48fc62396b70c46bce315519bf2dfe08
unknown <priyasonar.2009@gmail.com>
2024-07-23 00:54:27 +0530

    logs for allowed origin

--- functions/api.js
+++ functions/api.js
@@ -3,11 +3,13 @@ import cors from 'cors';
 import Top5Router from "./routes/Top5Router.js";
 import GenAiRouter from "./routes/genAiRouter.js";
 import ServerlessHttp from "serverless-http";
+import { verifyOrigin } from './middleware/verify-origin.js';
 import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
+app.use(verifyOrigin);
 app.use('/.netlify/functions/api/top5', Top5Router);
 app.use('/.netlify/functions/api/gen-ai', GenAiRouter);
 app.get('/.netlify/functions/api/hello-world', (req, res) => {
@@ -15,7 +17,7 @@ app.get('/.netlify/functions/api/hello-world', (req, res) => {
         messages: "hello world!"
     })
 });
-
+// export default app;
 const serverlessHandler = ServerlessHttp(app);
 
 export const handler = async (event, context) => {
--- functions/middleware/verify-origin.js
+++ functions/middleware/verify-origin.js
@@ -1,10 +1,12 @@
 
 export const verifyOrigin = (req, res, next) => {
     const origin = req.get('Origin');
-
+    console.log("Requested Origin---->", origin);
     if (origin && origin === process.env.ALLOWED_ORIGIN) {
+        console.log("Allowed-->", origin);
         next();
     } else {
+        console.log("Invalid Origin-->", origin);
         res.status(403).json({ message: 'Forbidden: Invalid Origin' });
     }
 }
